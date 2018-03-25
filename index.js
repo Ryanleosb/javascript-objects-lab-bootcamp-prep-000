4a75e024606506e31d959cecf809c35599b0ece5
leo bang <ryanleosb@gmail.com>
2018-03-25 18:30:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,12 @@ var obj = { prop: 1 }
  function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({},obj,{[key]:value})
 };
+
+function destrictivelyUpdateObjectWithKeyAndValue (object,key value) {
+  object [key] = value
+  return object
+};
+function deleteFromObjectByKey (object,key) {
+  delete object.key
+  return key
+};
