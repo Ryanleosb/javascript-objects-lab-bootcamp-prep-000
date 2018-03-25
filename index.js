a99a270d8783db62a79fd9f399ae0b88292c86e9
leo bang <ryanleosb@gmail.com>
2018-03-25 18:36:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,14 @@ function destrictivelyUpdateObjectWithKeyAndValue (object,key, value) {
   object [key] = value
   return object
 };
+
 function deleteFromObjectByKey (object, key) {
   delete object.key
   return key
 };
+
+function destructivelyUpdateObjectWithKeyAndValue (object, key, value) {
+  object [key] = value
+  return object
+}
+
