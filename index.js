ec553e29e1ac5c52c78aaf891c0ae21dd1c0095c
leo bang <ryanleosb@gmail.com>
2018-03-25 18:37:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,4 +18,9 @@ function deleteFromObjectByKey (object, key) {
 function destructivelyUpdateObjectWithKeyAndValue (object, key, value) {
   object [key] = value
   return object
+};
+
+function destructivelyDeleteFromObjectByKey (object, key) {
+  delete object.key
+  return object
 }
